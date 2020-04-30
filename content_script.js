3bb521a5a0972f4a41fe5ccef2dfd0ee2a730625
Sen Palanisami <sen@spenthil.com>
2020-04-29 20:41:50 -0700

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,3 +1,6 @@
+console.log("[IVC] Looking for video elements.");
 Array.from(document.getElementsByTagName('video')).forEach((video) => {
   video.style.pointerEvents = 'none'
+  console.log("[IVC] Updated video element.", video);
 });
+console.log("[IVC] Done looking for video elements.");
